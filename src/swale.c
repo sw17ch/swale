316493330c07a33ef36802771a87986d0bc2ed78
John Van Enk <vanenkj@gmail.com>
2012-08-27 21:08:41 -0400

    SwaleTaskInit

--- src/swale.c
+++ src/swale.c
@@ -1,1 +1,11 @@
+#include <stdlib.h>
+
 #include "swale.h"
+
+void SwaleTaskInit(swale_task_t * task,
+                   swale_entry_cb_t entry,
+                   void * param) {
+  task->next = NULL;
+  task->entry = entry;
+  task->param = param;
+}
--- test/test_swale.c
+++ test/test_swale.c
@@ -1,15 +1,34 @@
 #include "unity.h"
 #include "swale.h"
 
-void setUp(void)
-{
+#include "mock_cb_stubs.h"
+
+void setUp(void) { }
+
+void tearDown(void) { }
+
+void test_SwaleProcess(void) {
+	TEST_IGNORE_MESSAGE("Implement me!");
 }
 
-void tearDown(void)
-{
+void test_SwaleTaskInit_fills_in_a_task(void) {
+  swale_task_t task;
+  uint32_t a_param = 99;
+
+  SwaleTaskInit(&task, task_entry_0, (void*)&a_param);
+
+  TEST_ASSERT_EQUAL_PTR(NULL, task.next);
+  TEST_ASSERT_EQUAL_PTR(task_entry_0, task.entry);
+  TEST_ASSERT_EQUAL_PTR((void*)&a_param, task.param);
+
+  /* Ensure we catch when the size of swale_task_t changes. */
+  TEST_ASSERT_EQUAL(24, sizeof(swale_task_t));
 }
 
-void test_module_generator_needs_to_be_implemented(void)
-{
+void test_SwaleRegisterTask_should_set_head_and_tail_if_no_head(void) {
+	TEST_IGNORE_MESSAGE("Implement me!");
+}
+
+void test_SwaleRegisterTask_should_replace_tail_if_head_exists(void) {
 	TEST_IGNORE_MESSAGE("Implement me!");
 }
